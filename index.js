72cc6b785477ea136b1349846e6e2d000fe20ef9
tommiHAMK <tommi.saksa@hamk.fi>
2026-04-08 09:37:37 +0300

    home root

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 
 const app = express();
 
+app.engine('handlebars', exphbs.engine({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
 app.get('/', (req,res) => {
-    res.send("Testing");
+    res.render('index');
 });
 
 const PORT = process.env.PORT || 5000;
